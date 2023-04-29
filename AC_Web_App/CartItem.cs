ba800e35642bb3f85b07b182586c5a01ef09225c
BarberRyan <Meoco55@gmail.com>
2023-04-29 03:44:51 -0400

    changed getcart "qty" to "cartQty."

--- AC_Web_App/CartItem.cs
+++ AC_Web_App/CartItem.cs
@@ -4,14 +4,14 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Qty { get; set; }
+        public int CartQty { get; set; }
         public List<string>? ImageNames { get; set; }
 
         public CartItem(int ID, string name, int qty)
         {
             Id = ID;
             Name = name;
-            Qty = qty;
+            CartQty = qty;
         }
 
         public void AddImage(string filename)
